b6498c51bfc035de7a6b9edfe6f6eb0bce61436e
CharanCJ <charantejqv@gmail.com>
2024-08-02 16:14:55 +0530

    added hike calculator program

---
+++ hike_calculator.py
@@ -0,0 +1,16 @@
+class sal:
+    def est_sal(self,cur_sal,years,avg_hike):
+        self.cur_sal = cur_sal
+        self.years = years
+        self.avg_hike = avg_hike
+        import datetime as dt
+        now = dt.datetime.now().strftime("%Y")
+        for y in range(years):
+            increment_sal = (cur_sal/100) * avg_hike
+            total_sal = round(float(cur_sal) + float(increment_sal))
+            cur_sal = total_sal
+            now = int(now) + 1
+            print("In the year",now,"Expected Salary is:", total_sal)
+
+sal = sal()
+sal.est_sal(2550000,6,12)
